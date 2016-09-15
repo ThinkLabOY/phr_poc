e094e5f58d3ac5f9ccb94c7aed0def0ed7707119
raul <raulpiiber@gmail.com>
2016-09-15 13:01:46 +0300

    Docker starts hbase and spring applications automatically

--- src/main/docker/start-pseudo-distributed.sh
+++ src/main/docker/start-pseudo-distributed.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo "starting Spring application"
+java -jar -Djava.security.egd="file:/dev/./urandom" /app.jar & apid=$!;   #-DFOREGROUND
+echo "apid: " + apid
+
+echo "starting HBase"
 /opt/hbase/bin/hbase zookeeper > logzoo.log 2>&1 &
 /opt/hbase/bin/hbase regionserver start > logregion.log 2>&1 &
 
 /opt/hbase/bin/hbase master start --localRegionServers=0
+
+echo "startup done"
+
